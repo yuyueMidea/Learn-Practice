414bf8964fe8613a4d3c2688dbfcd8f7964f051f
苍穹勇敢的心 <18825237023@163.com>
2025-04-26 16:50:40 +0800

    修改并添加一些统一的样式

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -10,8 +10,8 @@ import './App.css'
 import UserInfo from './UserInfo.jsx';
 import GoodsList from './GoodsList.jsx'
 import Signup from './Signup.jsx'
-import Test from './Test.jsx'
-import Test2 from './Test2.jsx'
+import SiblingsParam from './SiblingsParam.jsx'
+import Refdemo from './Refdemo.jsx'
 import Score from "./Score.jsx";
 import Toolbar from "./Toolbar.jsx";
 import Movingdot from './Movingdot'
@@ -20,23 +20,23 @@ export default function AppCopy() {
     return(
         <BrowserRouter>
             <nav className='navbar'>
-                <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/">首页</NavLink>
-                <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/userinfo">用户</NavLink>
-                <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/goodslist">商品</NavLink>
-                <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/test">Test</NavLink>
-                <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/test2">Test2</NavLink>
+                <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/" title='home'>首页</NavLink>
+                <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/userinfo" title='渲染用户'>用户</NavLink>
+                <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/goodslist" title='渲染商品列表'>商品</NavLink>
+                <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/siblingsParam" title='兄弟组件传值'>SiblingsParam</NavLink>
+                <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/refDemo" title='Ref用法'>Refdemo</NavLink>
                 <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/signup">signup</NavLink>
                 <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/score">測試渲染</NavLink>
-                <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/toolbar">事件傳播</NavLink>
-                <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/movingdot">Movingdot</NavLink>
+                <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/toolbar" title='事件冒泡'>事件傳播</NavLink>
+                <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/movingdot" title='设置对象state'>Movingdot</NavLink>
                 <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/checkedItems">CheckedItems</NavLink>
             </nav>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/userinfo" element={<UserInfo />} />
                 <Route path="/goodslist" element={<GoodsList />} />
-                <Route path="/test" element={<Test />} />
-                <Route path="/test2" element={<Test2 />} />
+                <Route path="/siblingsParam" element={<SiblingsParam />} />
+                <Route path="/refDemo" element={<Refdemo />} />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/score" element={<Score />} />
                 <Route path="/toolbar" element={<Toolbar />} />
--- vite-react/src/Toolbar.jsx
+++ vite-react/src/Toolbar.jsx
@@ -1,19 +1,19 @@
 export default function Toolbar() {
     function handleClick1() {
-        
-        alert('=btn1, handleClick1=')
+        alert('=点击了按钮一=')
     }
     function handleClick2() {
-        alert('=btn2,handleClick222=')
+        alert('=点击了按钮二=')
     }
     return(
         <div onClick={()=> {alert('click-toolbar')}} style={{'background': 'grey', padding:'5px'}}>
-            {/* 按鈕点击的时候，阻止事件传播，调用 e.stopPropagation()，阻止事件进一步冒泡 */}
-            <button onClick={e=>{
+            <p>按鈕点击的时候，阻止事件传播，调用 e.stopPropagation()，阻止事件进一步冒泡</p>
+            
+            <button className="btn" onClick={e=>{
                 e.stopPropagation()
                 handleClick1()
-            }}>btn1</button>
-            <button onClick={handleClick2}>btn2</button>
+            }}>按钮一（阻止冒泡）</button>
+            <button className="btn" onClick={handleClick2}>按钮二</button>
         </div>
     )
 }
