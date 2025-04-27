3e4e48f74ccd4d2f710ee9202b552b6ec811a4ee
苍穹勇敢的心 <18825237023@163.com>
2025-04-27 12:42:57 +0800

    新增Todolist菜单和功能，测试state状态逻辑相关的

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -6,6 +6,8 @@ import {
     NavLink,
     Navigate 
 } from 'react-router-dom';
+import {KeepAlive,AliveScope } from 'react-activation';
+
 import './App.css'
 import UserInfo from './UserInfo.jsx';
 import GoodsList from './GoodsList.jsx'
@@ -16,33 +18,38 @@ import Score from "./Score.jsx";
 import Toolbar from "./Toolbar.jsx";
 import Movingdot from './Movingdot'
 import CheckedItems from './CheckedItems.jsx'
+import TaskList from './TaskList'
 export default function AppCopy() {
     return(
         <BrowserRouter>
-            <nav className='navbar'>
-                <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/" title='home'>首页</NavLink>
-                <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/userinfo" title='渲染用户'>用户</NavLink>
-                <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/goodslist" title='渲染商品列表'>商品</NavLink>
-                <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/siblingsParam" title='兄弟组件传值'>SiblingsParam</NavLink>
-                <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/refDemo" title='Ref用法'>Refdemo</NavLink>
-                <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/signup">signup</NavLink>
-                <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/score">測試渲染</NavLink>
-                <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/toolbar" title='事件冒泡'>事件傳播</NavLink>
-                <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/movingdot" title='设置对象state'>Movingdot</NavLink>
-                <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/checkedItems">CheckedItems</NavLink>
-            </nav>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/userinfo" element={<UserInfo />} />
-                <Route path="/goodslist" element={<GoodsList />} />
-                <Route path="/siblingsParam" element={<SiblingsParam />} />
-                <Route path="/refDemo" element={<Refdemo />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/score" element={<Score />} />
-                <Route path="/toolbar" element={<Toolbar />} />
-                <Route path="/movingdot" element={<Movingdot />} />
-                <Route path="/checkedItems" element={<CheckedItems />} />
-            </Routes>
+            <AliveScope>
+                <nav className='navbar'>
+                    <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/" title='home'>首页</NavLink>
+                    <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/userinfo" title='渲染用户'>用户</NavLink>
+                    <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/goodslist" title='渲染商品列表'>商品</NavLink>
+                    <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/siblingsParam" title='兄弟组件传值'>SiblingsParam</NavLink>
+                    <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/refDemo" title='Ref用法'>Refdemo</NavLink>
+                    <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/signup">signup</NavLink>
+                    <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/score">測試渲染</NavLink>
+                    <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/toolbar" title='事件冒泡'>事件傳播</NavLink>
+                    <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/movingdot" title='设置对象state'>Movingdot</NavLink>
+                    <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/checkedItems">CheckedItems</NavLink>
+                    <NavLink className={({ isActive }) => isActive ? 'nav-item active' : 'nav-item'} to="/tasklist">TaskList</NavLink>
+                </nav>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/userinfo" element={ <UserInfo /> } />
+                    <Route path="/goodslist" element={ <GoodsList /> } />
+                    <Route path="/siblingsParam" element={<SiblingsParam />} />
+                    <Route path="/refDemo" element={<Refdemo />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/score" element={<Score />} />
+                    <Route path="/toolbar" element={<Toolbar />} />
+                    <Route path="/movingdot" element={<Movingdot />} />
+                    <Route path="/checkedItems" element={<CheckedItems />} />
+                    <Route path="/tasklist" element={<TaskList />} />
+                </Routes>
+            </AliveScope>
         </BrowserRouter>
     )
 }
---
+++ vite-react/src/TaskList.jsx
@@ -0,0 +1,87 @@
+import { useState } from "react"
+const initialTask = [
+    {id:0, text:'task0', done: true, isEditing: false},
+    {id:1, text:'task1', done: false, isEditing: false},
+    {id:2, text:'task2', done: false, isEditing: false},
+]
+let nextid = 3;
+export default function TaskList() {
+    const [showFlag, setshowFlag] = useState(false);
+    const [inputtext, settext] = useState('');
+    const [task, settask] = useState(initialTask);
+    function handleChangeItem(item, newVal) {
+        console.warn('changeText: ', newVal, ', item: ',item)
+        settask(task.map(v=>{
+            if(v.id == item.id) {
+                v.text = newVal
+            }
+            return v
+        }))
+    }
+    function handleDeleteItem(cid) {
+        const filteredTask = task.filter(v=>v.id!==cid)
+        settask(filteredTask)
+    }
+    function handleSaveItem(cid) {
+        console.warn(cid, 'saveeee: ', task)
+        settask(task.map(v=>{
+            if(v.id == cid) {
+                v.isEditing = false
+            }
+            return v
+        }))
+    }
+    function handleEditItem(cid) {
+        console.warn('editttt: ', cid)
+        settask(task.map(v=>{
+            if(v.id == cid) {
+                v.isEditing = true
+            }
+            return v
+        }))
+    }
+    function addOneTask() {
+        console.warn('addOneTask--', inputtext)
+        settask([
+            ...task,
+            {id:nextid++, text: inputtext, done: false, isEditing: false}
+        ])
+    }
+    function generateTask() {
+        console.log('taskkkk: ', task)
+        setshowFlag(true);
+        setTimeout(() => {
+            setshowFlag(false);
+        }, 3000);
+    }
+    function handleChkChange(cid, e) {
+        console.warn(cid, ', chked: ', e.target.checked )
+        settask(task.map(v=>{
+            if(v.id == cid) {
+                v.done = e.target.checked
+            }
+            return v
+        }))
+    }
+    return(
+        <>
+            <h2>To do list</h2>
+            <p style={{margin:5,padding:5}}>
+                <input className="input showInput" type="text" value={inputtext} onChange={e=>settext(e.target.value)} />
+                <button onClick={addOneTask} disabled={inputtext===''}>添加任务</button>
+                <button className="btn" onClick={generateTask} disabled={task.length==0}>生成已经勾选的任务</button>
+                <span className="showTxt">{showFlag ? task.filter(v=>v.done).map(v=>v.text).join(', ') :''}</span>
+            </p>
+            <ul className="listWrapper">
+                {task.map(item=>
+                    <li key={item.id}>
+                        <input type="checkbox" id={item.id} checked={item.done} onChange={e=>handleChkChange(item.id, e)} />
+                        {item.isEditing ? <input className="input showInput" type="text" value={item.text} onChange={e=>handleChangeItem(item, e.target.value)} /> : <label htmlFor={item.id} className="showTxt">{item.text}</label> }
+                        {item.isEditing ? <button className="btn" onClick={()=>{handleSaveItem(item.id)}}>保存({item.id})</button>: <button className="btn" onClick={()=>{handleEditItem(item.id)}}>编辑({item.id})</button>}
+                        <button className="btn" onClick={()=>{handleDeleteItem(item.id)}}>删除({item.id})</button>
+                    </li>
+                )}
+            </ul>
+        </>
+    )
+}
