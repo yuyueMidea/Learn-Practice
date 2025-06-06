7d915e90f40538fe2e9e48231778e08c7a707a75
苍穹勇敢的心 <18825237023@163.com>
2025-06-06 22:17:25 +0800

    顶部状态条，添加用户头像图标

--- vue3-ts/components/layout/AppHeader.vue
+++ vue3-ts/components/layout/AppHeader.vue
@@ -1,34 +1,71 @@
 <template>
-    <header class="app-header">
-      <button @click="toggleSidebar">
-        <i class="icon-menu"></i>
-      </button>
-      <h1>{{ title }}</h1>
-    </header>
-  </template>
+  <header class="app-header">
+    <button class="toggle_btn" @click="toggleSidebar">
+      <Expand/>
+      <!-- <i class="icon-menu"></i> -->
+    </button>
+    <h3 class="title_wrapper">{{ title }}</h3>
+    <div class="userinfo-wrapper" v-if="authStore.isAuthenticated">
+      <Avatar/>
+      <span>欢迎, {{ authStore.currentUser?.username }}</span>
+      <span>密码等级: {{ appStore.passwordLevel }}</span>
+      <button @click="handleLogout">退出登录</button>
+
+    </div>
+  </header>
+</template>
   
-  <script lang="ts" setup>
-  import { computed } from 'vue'
-  import { useRoute } from 'vue-router'
-  import { useAppStore } from '../../stores/app'
+<script lang="ts" setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import { useAppStore } from '../../stores/app'
+import { useAuthStore } from '../../stores/authStore'
+import router from '../../router'
+import { Expand, User, Avatar } from '@element-plus/icons-vue'
+const route = useRoute()
+const appStore = useAppStore()
+const authStore = useAuthStore();
+const title = computed(() => route.meta.title as string)
+
+const toggleSidebar = () => {
+  appStore.toggleSidebar()
+}
+const handleLogout = ()=>{
+  //退出登录，并将密码等级重置0
+  authStore.logout();
+  appStore.setPassword(0);
+  router.push('/login');
+}
+</script>
   
-  const route = useRoute()
-  const appStore = useAppStore()
-  
-  const title = computed(() => route.meta.title as string)
-  
-  const toggleSidebar = () => {
-    appStore.toggleSidebar()
-  }
-  </script>
-  
-  <style scoped>
-  .app-header {
-    display: flex;
-    align-items: center;
-    padding: 0 20px;
-    height: 60px;
-    background-color: #2c3e50;
-    color: white;
-  }
-  </style>
+<style scoped>
+.app-header {
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  height: 60px;
+  background-color: #2c3e50;
+  color: white;
+}
+.app-header .toggle_btn {
+  padding: 0;
+}
+.app-header .toggle_btn svg{
+  width: 40px;
+  height: 20px;
+}
+.title_wrapper {
+  margin-left: 22px;
+}
+.userinfo-wrapper {
+  position: fixed;
+  right: 22px;
+}
+.userinfo-wrapper svg{
+  width: 22px;
+}
+.userinfo-wrapper span {
+  display: inline-block;
+  margin: 20px;
+}
+</style>
