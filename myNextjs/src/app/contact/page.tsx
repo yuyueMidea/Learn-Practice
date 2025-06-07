bc27d6c6e1327a6ff3537af5df63ff395a58eb1e
苍穹勇敢的心 <18825237023@163.com>
2025-06-07 23:28:07 +0800

    Delete myNextjs/src/app/contact/page.tsx

--- myNextjs/src/app/contact/page.tsx
+++
@@ -1,8 +0,0 @@
-import styles from './styles.module.css'
-export default function Contact() {
-    return (
-        <div className= {styles.wrapper}>
-            <h1>Contact page</h1>
-        </div>
-    )
-}
