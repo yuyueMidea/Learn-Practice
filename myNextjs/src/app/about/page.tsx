98759df2f88beccf78313b3f3c6fd020761237d4
苍穹勇敢的心 <18825237023@163.com>
2025-06-07 23:28:39 +0800

    Delete myNextjs/src/app/about/page.tsx

--- myNextjs/src/app/about/page.tsx
+++
@@ -1,8 +0,0 @@
-import styles from './styles.module.css'
-export default function About() {
-    return (
-        <div className= {styles.wrapper}>
-            <h1>About page</h1>
-        </div>
-    )
-}
