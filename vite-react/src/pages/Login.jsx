28a4ee437aace05bfef42ee0249cfe34b4819b3b
苍穹勇敢的心 <18825237023@163.com>
2025-06-11 23:46:22 +0800

    登录界面，使用 Tailwind CSS 实现简单加载动画

--- vite-react/src/pages/Login.jsx
+++ vite-react/src/pages/Login.jsx
@@ -44,8 +44,16 @@ const Login = () => {
         className='border p-2 m-2 rounded shadow-2xs'
       />
       <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition m-4">Login</button>
-      <h1 className='text-green-300 p-2 m-2'>{ loading ? 'Loading...' : '' }</h1>
-      <h1 className='text-red-500 p-2 m-2'>{ errorTxt }</h1>
+      {/* <h1 className='text-green-300 p-2 m-2'>{ loading ? 'Loading...' : '' }</h1> */}
+      <div class="bg-white p-6 rounded-lg shadow">
+        { loading ?
+          <div class="flex justify-center items-center">
+            <div class="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+          </div>
+          : 'Please Login'
+        }
+      </div>
+      <div className='bg-white p-6 rounded-lg shadow text-red-500 m-2'>{ errorTxt }</div>
     </form>
   );
 };
