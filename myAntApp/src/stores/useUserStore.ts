d98f925bdf7cd9011643e04b19075189f6b70a97
苍穹勇敢的心 <18825237023@163.com>
2025-06-20 21:41:26 +0800

    useStore存储模块添加按角色查询列表的方法

--- myAntApp/src/stores/useUserStore.ts
+++ myAntApp/src/stores/useUserStore.ts
@@ -11,6 +11,7 @@ interface UserStore {
     addUser: (user: User) => void;
     updateUser: (user: User) => void;
     deleteUser: (id: string) => void;
+    getUsersByRole: (role: User['role']) => User[];
 }
 export const useUserStore = create<UserStore>((set, get) =>({
     users: [
@@ -31,5 +32,9 @@ export const useUserStore = create<UserStore>((set, get) =>({
     // 删除用户
     deleteUser: (cid) =>{
         set(state => ({ users: state.users.filter(v=>v.id!==cid) }) )
+    },
+    // 根据角色筛选用户
+    getUsersByRole: (role) => {
+        return get().users.filter(user => user.role === role);
     }
 }))
