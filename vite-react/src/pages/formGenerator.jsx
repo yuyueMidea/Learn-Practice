070695069005fafd3e00346e62963be74e9d273f
苍穹勇敢的心 <18825237023@163.com>
2025-06-21 16:55:29 +0800

    新增表单设计界面

---
+++ vite-react/src/pages/formGenerator.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Button } from 'antd';
+import FormRender, { useForm } from 'form-render';
+
+const schema = {
+  type: 'object',
+  properties: {
+    input1: {
+      title: '简单输入框',
+      type: 'string',
+      required: true,
+    },
+    select1: {
+      title: '单选',
+      type: 'string',
+      enum: ['a', 'b', 'c'],
+      enumNames: ['早', '中', '晚'],
+    },
+  },
+};
+
+const Demo = () => {
+  const form = useForm();
+  const onFinish = (formData, errors) => {
+    console.log('formData:', formData, 'errors', errors);
+  };
+  return (
+    <div>
+      <FormRender form={form} schema={schema} onFinish={onFinish} />
+      <Button type="primary" onClick={form.submit}>
+        提交
+      </Button>
+    </div>
+  );
+};
+
+export default Demo;
