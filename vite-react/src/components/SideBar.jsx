b180d0796461941e87944badb30956214d54b914
苍穹勇敢的心 <18825237023@163.com>
2025-06-21 22:30:22 +0800

    顶部状态栏 和左侧菜单栏的一些信息从store中获取

--- vite-react/src/components/SideBar.jsx
+++ vite-react/src/components/SideBar.jsx
@@ -1,35 +1,24 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
+import useCounterStore from '../stores/counterStore'
+import { useEffect } from 'react';
 
 export default function Sidebar({ open }) {
-    // 定义菜单项
-    const menuItems = [
-        { path: '/', name: '首页' },
-        { path: '/userinfo', name: '用户' },
-        // { path: '/login', name: '登录' },
-        { path: '/goodslist', name: '商品' },
-        { path: '/siblingsParam', name: '兄弟组件传值' },
-        { path: '/refDemo', name: 'Ref用法' },
-        { path: '/cacheDemo', name: '缓存测试' },
-        { path: '/toolbar', name: '事件冒泡' },
-        {path: '/signup', name: '注册'},
-        { path: '/movingdot', name: 'Movingdot' },
-        { path: '/GrandSon', name: 'GrandSon' },
-        { path: '/checkedItems', name: 'CheckedItems' },
-        { path: '/taskList', name: '任务列表' },
-        { path: '/toggleSize', name: 'ToggleSize' },
-        { path: '*', name: 'NotFound' },
-    ]
-  /* const menuItems = [
-    { path: '/', name: '首页', icon: 'M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6' },
-    { path: '/dashboard', name: '仪表盘', icon: 'M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z' },
-    { path: '/settings', name: '设置', icon: 'M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z' },
-  ] */
+  const menuList = useCounterStore(state => state.menuNameList)
+  const setcMenuName = useCounterStore(state => state.setMenuName)
+  // console.log('sidebar_menuList_: ', menuList )
+  const location = useLocation();
+  
+  useEffect(() => {
+    const cname =( menuList.find(v => v.path ===location.pathname) || {}).name
+    console.log("Route changed to:", location.pathname, cname);
+    setcMenuName(cname)
+  }, [location]);
 
   return (
     <aside className={`bg-gray-800 text-white ${open ? 'w-64' : 'w-20'} transition-all duration-300 ease-in-out flex-shrink-0 overflow-y-auto yy3aside`}>
       <nav className="mt-4">
         <ul>
-          {menuItems.map((item) => (
+          {menuList.map((item) => (
             <li key={item.path} className="px-2 py-1">
               <NavLink
                 to={item.path}
--- vite-react/src/components/TopBar.jsx
+++ vite-react/src/components/TopBar.jsx
@@ -1,5 +1,7 @@
 import { useLocation, useNavigate } from "react-router-dom"
+import useCounterStore from "../stores/counterStore";
 export default function TopBar({ sidebarOpen, setSidebarOpen, userInfo, quitEvent }) {
+  const cmenuName = useCounterStore(state => state.menuName);
   const navigate = useNavigate();
   const location = useLocation();
     return (
@@ -22,7 +24,7 @@ export default function TopBar({ sidebarOpen, setSidebarOpen, userInfo, quitEven
           </button>
           <span>{ location.pathname }</span>
           {/* 标题 */}
-          <h1 className="text-xl font-semibold text-gray-800">Vite + React, demo</h1>
+          <h1 className="text-xl font-semibold text-gray-800">{ cmenuName }</h1>
           
           {/* 用户信息/通知等 */}
           <div className="flex items-center space-x-4">
@@ -31,13 +33,13 @@ export default function TopBar({ sidebarOpen, setSidebarOpen, userInfo, quitEven
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
               </svg>
             </button>
-            <div className="flex items-center">
-              <span className="text-gray-700 mr-2">{userInfo}</span>
-              <div className="h-8 w-8 rounded-full bg-blue-500 flex items-center justify-center text-white">
-                U
+            {userInfo ? 
+              <div className="flex items-center">
+                <span className="text-gray-700 mr-2 border-b-2">Welcome, {userInfo}</span>
+                <button onClick={()=> {quitEvent(); navigate('/login')}} className="border rounded p-1 m-2 hover:bg-red-600 transition">退出登录</button>
               </div>
-              <button onClick={()=> {quitEvent(); navigate('/login')}} className="border rounded p-1 m-2 hover:bg-red-600 transition">退出登录</button>
-            </div>
+              :<div>Please login</div>
+            }
           </div>
         </div>
       </header>
