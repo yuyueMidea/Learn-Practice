e5c42b4ddd6c4c80ad0d91929bc5dc6c415583dc
苍穹勇敢的心 <18825237023@163.com>
2025-07-19 09:55:10 +0800

    Add files via upload

--- Electron/electron_vue3/src/renderer/src/menus.js
+++ Electron/electron_vue3/src/renderer/src/menus.js
@@ -3,20 +3,20 @@ export const menuItems = [
       path: '/',
       name: '首页',
       icon: '🏠',
-      component: () => import('./views/Home.vue')
+      component: () => import('@renderer/views/Home.vue')
     },
-    {path: '/login', name: '登录', icon: 'ℹ️', component: () => import('./views/LoginPage.vue')},
+    {path: '/login', name: '登录', icon: 'ℹ️', component: () => import('@renderer/views/LoginPage.vue')},
     {
       path: '/settings',
       name: '设置',
       icon: '⚙️',
-      component: () => import('./views/Settings.vue')
+      component: () => import('@renderer/views/Settings.vue')
     },
     {
       path: '/userList',
       name: '用户列表',
       icon: 'ℹ️',
-      component: () => import('./views/UserList.vue')
+      component: () => import('@renderer/views/UserList.vue')
     }
   ];
   
