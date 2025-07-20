0b38001a8c15d42e18a4141a59939d3576e73906
苍穹勇敢的心 <18825237023@163.com>
2025-07-20 15:22:13 +0800

    新增下载json工具类，

---
+++ Electron/electron_vue3/src/renderer/src/utils/download.js
@@ -0,0 +1,9 @@
+export function downloadJSON(data, filename = 'form-schema.json') {
+  const blob = new Blob([JSON.stringify(data, null, 2)], { type: 'application/json' })
+  const url = URL.createObjectURL(blob)
+  const a = document.createElement('a')
+  a.href = url
+  a.download = filename
+  a.click()
+  URL.revokeObjectURL(url)
+}
