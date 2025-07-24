c375ed58ea20039b518efa7ec2109a63d61886ac
苍穹勇敢的心 <18825237023@163.com>
2025-07-24 13:39:19 +0800

    统一导出（components/index.js）

---
+++ vue3-dataserver/client/src/components/index.js
@@ -0,0 +1,9 @@
+import BaseButton from "./BaseButton.vue";
+import BaseModal from "./BaseModal.vue";
+import BaseTable from "./BaseTable.vue";
+
+export {
+    BaseButton,
+    BaseModal,
+    BaseTable
+}
