279e7fd5e68b496efca218477cf20d0b1ee9dc74
苍穹勇敢的心 <18825237023@163.com>
2025-08-09 21:39:50 +0800

    更新 menuList.js（加图标）

--- react/react-mui-demo/src/menuList.js
+++ react/react-mui-demo/src/menuList.js
@@ -6,24 +6,9 @@ import PeopleIcon from '@mui/icons-material/People';
 import InfoIcon from '@mui/icons-material/Info';
 
 const menuList = [
-  {
-    path: '/',
-    label: '首页',
-    icon: <HomeIcon />,
-    element: <Home />
-  },
-  {
-    path: '/users',
-    label: '用户管理',
-    icon: <PeopleIcon />,
-    element: <Users />
-  },
-  {
-    path: '/about',
-    label: '关于',
-    icon: <InfoIcon />,
-    element: <About />
-  }
+  { path: '/', label: '首页', icon: <HomeIcon />, element: <Home /> },
+  { path: '/users', label: '用户管理', icon: <PeopleIcon />, element: <Users /> },
+  { path: '/about', label: '关于', icon: <InfoIcon />, element: <About /> }
 ];
 
 export default menuList;
