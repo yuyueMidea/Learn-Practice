4d12a3cc79f70b9d7cfcb40e27d7d78ecc9f8531
苍穹勇敢的心 <18825237023@163.com>
2025-08-16 23:43:40 +0800

    Add files via upload

--- myNextjs/src/app/login/page.tsx
+++ myNextjs/src/app/login/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Link from "next/link";
-import styles from './styles.module.css'
 import { useState } from "react";
 import useAuthStore from "../store/authStore";
 import { redirect } from "next/navigation";
@@ -12,39 +11,40 @@ export default function LoginPage() {
     if(uname==='') return
     login({
       username: uname,
-      role: uname
+      password: pwd
     });
     redirect('/')
   }
   const { login } = useAuthStore();
   return (
-    <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded shadow">
-      <div className={styles.formWrapper}>
-        <div>
-          <label className={styles.formLabel}>Name</label>
-          <input
-            className={styles.formInput}
-            value={uname}
-            onChange={e => setuname(e.target.value)}
-          />
+    <div className="flex items-center justify-center m-12">
+      <div className="p-6 m-2 border rounded w-80">
+        <div className="space-y-4 max-w-sm mx-auto">
+          <div>
+            <label className="block text-sm font-medium mb-1">用户名</label>
+            <input
+              type="text" value={uname} onChange={e => setuname(e.target.value)}
+              className="w-full border rounded px-3 py-2 focus:outline-none focus:ring focus:ring-blue-300"
+            />
+          </div>
+          <div>
+            <label className="block text-sm font-medium mb-1">密码</label>
+            <input
+              type="password" value={pwd} onChange={e => setpwd(e.target.value)}
+              className="w-full border rounded px-3 py-2 focus:outline-none focus:ring focus:ring-blue-300"
+            />
+          </div>
+          <div className="flex justify-end">
+            <button onClick={handleLogin} className="p-2 w-full bg-blue-600 text-white rounded hover:bg-blue-700">
+              登录
+            </button>
+          </div>
+          <div className="flex justify-end">
+            <Link href="/" className="text-blue-600 hover:underline">
+              ← Back to home
+            </Link>
+          </div>
         </div>
-        <div>
-          <label className={styles.formLabel} htmlFor="pwd">Password</label>
-          <input
-            className={styles.formInput}
-            value={pwd}
-            onChange={e => setpwd(e.target.value)}
-          />
-        </div>
-        <div>
-          <label className={styles.formLabel}></label>
-          <button className={styles.submitBtn} onClick={() => handleLogin()}>Sign In</button>
-        </div>
-      </div>
-      <div className="mt-4 text-center text-sm">
-        <Link href="/" className="text-blue-600 hover:underline">
-          ← Back to home
-        </Link>
       </div>
     </div>
   );
