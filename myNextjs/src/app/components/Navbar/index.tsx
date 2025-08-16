bccc0d340b93786c3befac9d5c851b02f963bb16
苍穹勇敢的心 <18825237023@163.com>
2025-08-16 23:45:17 +0800

    顶部状态条和左侧菜单应用tailwindCSS样式

--- myNextjs/src/app/components/Navbar/index.tsx
+++ myNextjs/src/app/components/Navbar/index.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import useAuthStore from "@/app/store/authStore";
-import styles from "./styles.module.css";
 import { redirect } from "next/navigation";
 
 export default function Navbar() {
@@ -13,17 +12,17 @@ export default function Navbar() {
     }, 500);
   }
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.navContainer}>
-        <div className={styles.logo}>Next.js Demo --- yy3</div>
-        {/* 这里可以放用户信息，比如头像/用户名 */}
-        {isAuthenticated &&
-          <div className={styles.userInfo}>
-            <span>Hello, {currentUser.username}</span>
-            <button className={styles.logout} onClick={handleLogout}>退出</button>
-          </div>}
-        
+    <header className="h-14 bg-gray-800 text-white flex items-center justify-between px-4 shadow">
+      {/* 左侧 Logo / 标题 */}
+      <div className="text-lg font-semibold text-gray-300">Dashboard</div>
+
+      {/* 右侧 用户操作 */}
+      <div className="flex items-center gap-4">
+        {isAuthenticated && <div className="p-1">
+          <span className="p-1 m-2">Hello, {currentUser.username}</span>
+          <button className="px-1 m-1 bg-gray-200 text-gray-800 rounded hover:bg-gray-300" onClick={handleLogout}>退出</button>
+        </div>}
       </div>
-    </nav>
+    </header>
   );
 }
--- myNextjs/src/app/components/Sidebar/index.tsx
+++ myNextjs/src/app/components/Sidebar/index.tsx
@@ -1,31 +1,33 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { NAV_ITEMS } from "../../types/navigation";
-import styles from "./styles.module.css";
 import useAuthStore from "@/app/store/authStore";
+import { useState } from "react";
 
 export default function Sidebar() {
-  const pathname = usePathname();
-  const { isAuthenticated ,currentUser, logout } = useAuthStore();
+  const { isAuthenticated } = useAuthStore();
+  const [active, setActive] = useState("/");
 
   return (
-    <aside className={styles.sidebar_aside}>
-      <ul className={styles.sidebarMenu}>
-        {NAV_ITEMS.filter((item) => (isAuthenticated || !item.requiresAuth)).map((item) => (
-          <li key={item.path}>
-            <Link
-              href={item.path}
-              className={`${styles.sidebarLink} ${
-                pathname === item.path ? styles.active : ""
-              }`}
-            >
-              {item.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <aside className="w-60 bg-gray-900 text-gray-200 h-[calc(100vh-3.5rem)] flex flex-col p-4 shadow-lg overflow-y-auto">
+      <nav className="flex-1">
+        <ul className="space-y-2">
+          {NAV_ITEMS.filter((item) => (isAuthenticated ||!item.requiresAuth)).map((item) => (
+            <li key={item.path}>
+              <Link href={item.path} onClick={() => setActive(item.path)}
+              className={`flex items-center gap-3 px-3 py-2 rounded-lg cursor-pointer transition ${
+                active === item.path
+                  ? "bg-gray-700 text-white"
+                  : "text-gray-400 hover:bg-gray-800 hover:text-white"
+              }`}>{item.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div className="text-sm text-gray-500 mt-auto">
+        © 2025 ChinaSofti
+      </div>
     </aside>
   );
 }
