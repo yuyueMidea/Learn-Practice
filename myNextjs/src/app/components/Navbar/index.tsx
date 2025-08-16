0a9003470df89b5d154d8d886a5efed4c8af440d
苍穹勇敢的心 <18825237023@163.com>
2025-08-16 18:08:55 +0800

    Add files via upload

--- myNextjs/src/app/components/Navbar/index.tsx
+++ myNextjs/src/app/components/Navbar/index.tsx
@@ -1,14 +1,28 @@
 "use client";
 
+import useAuthStore from "@/app/store/authStore";
 import styles from "./styles.module.css";
+import { redirect } from "next/navigation";
 
 export default function Navbar() {
+  const { isAuthenticated ,currentUser, logout } = useAuthStore();
+  const handleLogout = ()=>{
+    logout();
+    setTimeout(() => {
+      redirect('/login')
+    }, 500);
+  }
   return (
     <nav className={styles.navbar}>
       <div className={styles.navContainer}>
         <div className={styles.logo}>Next.js Demo --- yy3</div>
         {/* 这里可以放用户信息，比如头像/用户名 */}
-        <div className={styles.userInfo}>Hello, User</div>
+        {isAuthenticated &&
+          <div className={styles.userInfo}>
+            <span>Hello, {currentUser.username}</span>
+            <button className={styles.logout} onClick={handleLogout}>退出</button>
+          </div>}
+        
       </div>
     </nav>
   );
--- myNextjs/src/app/components/Sidebar/index.tsx
+++ myNextjs/src/app/components/Sidebar/index.tsx
@@ -4,14 +4,16 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { NAV_ITEMS } from "../../types/navigation";
 import styles from "./styles.module.css";
+import useAuthStore from "@/app/store/authStore";
 
 export default function Sidebar() {
   const pathname = usePathname();
+  const { isAuthenticated ,currentUser, logout } = useAuthStore();
 
   return (
     <aside className={styles.sidebar_aside}>
       <ul className={styles.sidebarMenu}>
-        {NAV_ITEMS.filter((item) => !item.requiresAuth).map((item) => (
+        {NAV_ITEMS.filter((item) => (isAuthenticated || !item.requiresAuth)).map((item) => (
           <li key={item.path}>
             <Link
               href={item.path}
