3a910583184b494d7d11af6b074ebe36c4a36392
苍穹勇敢的心 <18825237023@163.com>
2025-08-16 23:42:57 +0800

    Add files via upload

--- myNextjs/src/app/goods/page.tsx
+++ myNextjs/src/app/goods/page.tsx
@@ -9,24 +9,15 @@ const items: Item[] = [
     { id: 1, name: 'Apple', price: 1.99 },
     { id: 2, name: 'Banana', price: 0.99 },
     { id: 3, name: 'Orange', price: 2.49 },
-    { id: 4, name: 'Apple', price: 1.99 },
-    { id: 5, name: 'Banana', price: 0.99 },
-    { id: 6, name: 'Orange', price: 2.49 },
-    { id: 7, name: 'Apple', price: 1.99 },
-    { id: 8, name: 'Banana', price: 0.99 },
-    { id: 9, name: 'Orange', price: 2.49 },
-    { id: 10, name: 'Apple', price: 1.99 },
-    { id: 11, name: 'Banana', price: 0.99 },
-    { id: 12, name: 'Orange', price: 2.49 },
 ];
 export default function Goods() {
     const arrlist1 = [1,2,3,4,5,6,7,8,9]
     return (
-        <div className= {styles.goodsWrapper}>
-            <h2>商品列表</h2>
-            <ul className={styles.gooddList}>
+        <div className='p-2 m-2'>
+            <h2 className="text-center p-2 m-2">商品列表</h2>
+            <ul className="divide-y divide-gray-200 max-w-md mx-auto bg-white shadow rounded">
                 {items.map((item) => (
-                    <li key={item.id}>
+                    <li key={item.id} className="px-4 py-2 hover:bg-gray-50">
                         <div className="goodItem">
                             <span>{item.name}</span> | 
                             <span>${item.price.toFixed(2)}</span>
