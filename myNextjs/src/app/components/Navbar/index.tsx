284d681b0a5dbf59521ac3e6a8964ecd7f0a1165
苍穹勇敢的心 <18825237023@163.com>
2025-08-16 17:23:48 +0800

    改造一下：顶部 Navbar只显示 logo + 用户信息。左侧 Sidebar显示所有路由菜单。

--- myNextjs/src/app/components/Navbar/index.tsx
+++ myNextjs/src/app/components/Navbar/index.tsx
@@ -1,31 +1,14 @@
 "use client";
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { NAV_ITEMS } from "../../types/navigation";
 import styles from "./styles.module.css";
 
 export default function Navbar() {
-  const pathname = usePathname();
-
   return (
     <nav className={styles.navbar}>
-      <div className={`container ${styles.navContainer}`}>
-        <div className={styles.logo}>Next.js Demo---yy3</div>
-        <ul className={styles.menu}>
-          {NAV_ITEMS.filter((item) => !item.requiresAuth).map((item) => (
-            <li key={item.path}>
-              <Link
-                href={item.path}
-                className={`${styles.link} ${
-                  pathname === item.path ? styles.active : ""
-                }`}
-              >
-                {item.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
+      <div className={styles.navContainer}>
+        <div className={styles.logo}>Next.js Demo --- yy3</div>
+        {/* 这里可以放用户信息，比如头像/用户名 */}
+        <div className={styles.userInfo}>Hello, User</div>
       </div>
     </nav>
   );
--- myNextjs/src/app/components/Sidebar/index.tsx
+++ myNextjs/src/app/components/Sidebar/index.tsx
@@ -2,34 +2,28 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { NavItem } from "../../types/navigation";
+import { NAV_ITEMS } from "../../types/navigation";
 import styles from "./styles.module.css";
 
-interface SidebarProps {
-  items: NavItem[];
-}
-
-export default function Sidebar({ items }: SidebarProps) {
+export default function Sidebar() {
   const pathname = usePathname();
 
   return (
-    <aside className={styles.sidebar}>
-      <nav>
-        <ul className={styles.menu}>
-          {items.map((item) => (
-            <li key={item.path}>
-              <Link
-                href={item.path}
-                className={`${styles.link} ${
-                  pathname === item.path ? styles.active : ""
-                }`}
-              >
-                {item.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
+    <aside className={styles.sidebar_aside}>
+      <ul className={styles.sidebarMenu}>
+        {NAV_ITEMS.filter((item) => !item.requiresAuth).map((item) => (
+          <li key={item.path}>
+            <Link
+              href={item.path}
+              className={`${styles.sidebarLink} ${
+                pathname === item.path ? styles.active : ""
+              }`}
+            >
+              {item.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </aside>
   );
 }
